785164e17f94039ce8a91e8038bc15aa1738ce1f
Jon Cram <webignition@gmail.com>
2023-01-13 17:38:32 +0000

    Create empty project

---
+++ tests/Integration/AbstractIntegrationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\WorkerClient\Tests\Integration;
+
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Psr7\HttpFactory;
+use PHPUnit\Framework\TestCase;
+use SmartAssert\ServiceClient\Client as ServiceClient;
+use SmartAssert\WorkerClient\Client;
+
+abstract class AbstractIntegrationTest extends TestCase
+{
+    protected static Client $client;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$client = new Client(
+            'http://localhost:9081',
+            self::createServiceClient(),
+        );
+    }
+
+    private static function createServiceClient(): ServiceClient
+    {
+        $httpFactory = new HttpFactory();
+
+        return new ServiceClient($httpFactory, $httpFactory, new HttpClient());
+    }
+}
