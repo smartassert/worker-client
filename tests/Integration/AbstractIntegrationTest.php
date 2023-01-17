faa194cb02675d55a8cae7185c914a49da53b8e7
Jon Cram <webignition@gmail.com>
2023-01-17 09:47:41 +0000

    Create data repository and factory for worker events (#11)

--- tests/Integration/AbstractIntegrationTest.php
+++ tests/Integration/AbstractIntegrationTest.php
@@ -9,10 +9,14 @@ use GuzzleHttp\Psr7\HttpFactory;
 use PHPUnit\Framework\TestCase;
 use SmartAssert\ServiceClient\Client as ServiceClient;
 use SmartAssert\WorkerClient\Client;
+use SmartAssert\WorkerClient\Tests\Services\DataRepository;
+use SmartAssert\WorkerClient\Tests\Services\WorkerEventFactory;
 
 abstract class AbstractIntegrationTest extends TestCase
 {
     protected static Client $client;
+    protected static DataRepository $dataRepository;
+    protected static WorkerEventFactory $workerEventFactory;
 
     public static function setUpBeforeClass(): void
     {
@@ -20,6 +24,14 @@ abstract class AbstractIntegrationTest extends TestCase
             'http://localhost:9080',
             self::createServiceClient(),
         );
+
+        self::$dataRepository = new DataRepository(
+            'pgsql:host=localhost;port=5432;dbname=worker-db;user=postgres;password=password!'
+        );
+
+        self::$dataRepository->removeAllData();
+
+        self::$workerEventFactory = new WorkerEventFactory(self::$dataRepository);
     }
 
     private static function createServiceClient(): ServiceClient
