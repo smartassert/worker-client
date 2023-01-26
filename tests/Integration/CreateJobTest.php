e52699d2b15d8988b0cfcd9a501d40f5e87299e9
Jon Cram <webignition@gmail.com>
2023-01-26 11:50:37 +0000

    Model job creation data in test environment

--- tests/Integration/CreateJobTest.php
+++ tests/Integration/CreateJobTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SmartAssert\WorkerClient\Tests\Integration;
 
 use SmartAssert\WorkerClient\Model\JobCreationError;
+use SmartAssert\WorkerClient\Tests\Model\JobCreationProperties;
 use SmartAssert\WorkerJobSource\Exception\InvalidManifestException;
 use SmartAssert\YamlFile\Collection\ArrayCollection;
 use SmartAssert\YamlFile\YamlFile;
@@ -13,72 +14,58 @@ class CreateJobTest extends AbstractIntegrationTest
 {
     public function testCreateManifestEmpty(): void
     {
-        $label = md5((string) rand());
-        $eventDeliveryUrl = 'http://example.com/event_delivery_url';
-        $maximumDurationInSeconds = 300;
-        $manifestPaths = [];
-        $sources = new ArrayCollection([]);
-
         self::expectExceptionObject(InvalidManifestException::createForEmptyContent());
 
-        self::$client->createJob($label, $eventDeliveryUrl, $maximumDurationInSeconds, $manifestPaths, $sources);
+        $this->makeCreateJobCall(JobCreationProperties::create());
     }
 
     public function testCreateJobSourceTestMissing(): void
     {
-        $label = md5((string) rand());
-        $eventDeliveryUrl = 'http://example.com/event_delivery_url';
-        $maximumDurationInSeconds = 300;
-        $manifestPaths = [
-            'test1.yml',
-            'test2.yml',
-        ];
-
-        $sources = new ArrayCollection([
-            YamlFile::create('/test1.yml', 'file content'),
-        ]);
+        $jobCreationProperties = JobCreationProperties::create()
+            ->withManifestPaths(['test1.yml', 'test2.yml'])
+            ->withSources(new ArrayCollection([YamlFile::create('/test1.yml', 'file content')]))
+        ;
 
         self::assertEquals(
             new JobCreationError('source/test/missing', ['path' => 'test2.yml']),
-            self::$client->createJob($label, $eventDeliveryUrl, $maximumDurationInSeconds, $manifestPaths, $sources)
+            $this->makeCreateJobCall($jobCreationProperties)
         );
     }
 
     public function testCreateJobJobAlreadyExists(): void
     {
-        $label = md5((string) rand());
-        $eventDeliveryUrl = 'http://example.com/event_delivery_url';
-        $maximumDurationInSeconds = 300;
-        $manifestPaths = [
-            'test1.yml',
-        ];
+        $jobCreationProperties = JobCreationProperties::create()
+            ->withManifestPaths(['test1.yml'])
+            ->withSources(new ArrayCollection([YamlFile::create('/test1.yml', 'file content')]))
+        ;
 
-        $sources = new ArrayCollection([
-            YamlFile::create('/test1.yml', 'file content'),
-        ]);
-
-        self::$client->createJob($label, $eventDeliveryUrl, $maximumDurationInSeconds, $manifestPaths, $sources);
+        $this->makeCreateJobCall($jobCreationProperties);
         self::assertEquals(
             new JobCreationError('job/already_exists', []),
-            self::$client->createJob($label, $eventDeliveryUrl, $maximumDurationInSeconds, $manifestPaths, $sources)
+            $this->makeCreateJobCall($jobCreationProperties),
         );
     }
 
     public function testCreateJobSuccess(): void
     {
-        $label = md5((string) rand());
-        $eventDeliveryUrl = 'http://example.com/event_delivery_url';
-        $maximumDurationInSeconds = 300;
-        $manifestPaths = [
-            'test1.yml',
-        ];
-
-        $sources = new ArrayCollection([
-            YamlFile::create('/test1.yml', 'file content'),
-        ]);
+        $jobCreationProperties = JobCreationProperties::create()
+            ->withManifestPaths(['test1.yml'])
+            ->withSources(new ArrayCollection([YamlFile::create('/test1.yml', 'file content')]))
+        ;
 
         self::assertNull(
-            self::$client->createJob($label, $eventDeliveryUrl, $maximumDurationInSeconds, $manifestPaths, $sources)
+            $this->makeCreateJobCall($jobCreationProperties)
+        );
+    }
+
+    private function makeCreateJobCall(JobCreationProperties $jobCreationProperties): ?JobCreationError
+    {
+        return self::$client->createJob(
+            $jobCreationProperties->label,
+            $jobCreationProperties->eventDeliveryUrl,
+            $jobCreationProperties->maximumDurationInSeconds,
+            $jobCreationProperties->manifestPaths,
+            $jobCreationProperties->sources,
         );
     }
 }
