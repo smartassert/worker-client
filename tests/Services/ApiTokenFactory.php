6acad1dc6a8ab07447360ebb97fdcb2158aa1d9f
Jon Cram <webignition@gmail.com>
2023-11-23 11:40:15 +0000

    Use users 3.1, service-client 6.0 (#60)

--- tests/Services/ApiTokenFactory.php
+++ tests/Services/ApiTokenFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SmartAssert\WorkerClient\Tests\Services;
 
 use SmartAssert\ServiceClient\Client as ServiceClient;
+use SmartAssert\TestAuthenticationProviderBundle\ApiKeyProvider;
 use SmartAssert\TestAuthenticationProviderBundle\ApiTokenProvider;
 use SmartAssert\TestAuthenticationProviderBundle\FrontendTokenProvider;
 use SmartAssert\UsersClient\Client as UsersClient;
@@ -23,7 +24,8 @@ class ApiTokenFactory
     {
         $usersClient = new UsersClient('http://localhost:9080', $this->serviceClient);
         $frontendTokenProvider = new FrontendTokenProvider(['user@example.com' => 'password'], $usersClient);
-        $apiTokenProvider = new ApiTokenProvider($usersClient, $frontendTokenProvider);
+        $apiKeyProvider = new ApiKeyProvider($usersClient, $frontendTokenProvider);
+        $apiTokenProvider = new ApiTokenProvider($usersClient, $apiKeyProvider);
 
         return $apiTokenProvider->get('user@example.com');
     }
